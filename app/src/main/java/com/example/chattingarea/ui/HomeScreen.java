383ea74bb903225543e910c24906e4747eaab2fc
trannam1312 <anhnam2k.yn@gmail.com>
2022-05-25 22:59:07 +0700

    update confic firebase + add ui chatgroup

--- app/src/main/java/com/example/chattingarea/ui/HomeScreen.java
+++ app/src/main/java/com/example/chattingarea/ui/HomeScreen.java
@@ -1,13 +1,12 @@
 package com.example.chattingarea.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.chattingarea.MainActivity;
 import com.example.chattingarea.R;
@@ -52,13 +51,14 @@ public class HomeScreen extends Fragment {
     }
 
     private void initAction() {
-        tvSignOut.setOnClickListener(view -> ((MainActivity)getActivity()).openLoginScreen());
+        tvSignOut.setOnClickListener(view -> ((MainActivity) getActivity()).openLoginScreen());
 
-        tvProfile.setOnClickListener(view -> ((MainActivity)getActivity()).openProfileScreen());
+        tvProfile.setOnClickListener(view -> ((MainActivity) getActivity()).openProfileScreen());
 
-        tvChatFriend.setOnClickListener(view -> ((MainActivity)getActivity()).openChatScreen());
+        tvChatFriend.setOnClickListener(view -> ((MainActivity) getActivity()).openChatScreen());
+
+        tvChatGroup.setOnClickListener(view -> ((MainActivity) getActivity()).openChatGroup());
     }
-
 
 
 }
