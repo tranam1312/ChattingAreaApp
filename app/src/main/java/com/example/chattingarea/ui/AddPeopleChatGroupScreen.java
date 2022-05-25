3847049f8974b9902d1a3dfd4f4e5969905f9e48
trannam1312 <anhnam2k.yn@gmail.com>
2022-05-26 01:16:04 +0700

    add people chat gruop

--- app/src/main/java/com/example/chattingarea/ui/AddPeopleChatGroupScreen.java
+++ app/src/main/java/com/example/chattingarea/ui/AddPeopleChatGroupScreen.java
@@ -1,17 +1,20 @@
 package com.example.chattingarea.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.chattingarea.R;
 
 public class AddPeopleChatGroupScreen extends Fragment {
-
+    private View view;
+    private RecyclerView recyclerView;
 
     public static AddPeopleChatGroupScreen newInstance(String param1, String param2) {
         AddPeopleChatGroupScreen fragment = new AddPeopleChatGroupScreen();
@@ -28,6 +31,17 @@ public class AddPeopleChatGroupScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_people_chat_group_screen, container, false);
+        return view = inflater.inflate(R.layout.fragment_add_people_chat_group_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        recyclerView = view.findViewById(R.id.user_rvc);
+
     }
 }
